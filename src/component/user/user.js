571d1ff19526d94aef7825880b62fb55976992d4
dongdongsong <songdongdong@peralppay.com>
2018-05-19 17:26:45 +0800

    dd

--- src/component/user/user.js
+++ src/component/user/user.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Result, List } from 'antd-mobile'
+import { Result, List, Button } from 'antd-mobile'
 import { connect } from 'react-redux'
 import './user.css'
 
@@ -7,12 +7,24 @@ import './user.css'
   state => state.user
 )
 class User extends React.Component{
+  constructor (props) {
+    super(props)
+    this.test = this.test.bind(this)
+    this.register = this.register.bind(this)
+  }
+  register () {
+    this.props.history.push('/register')
+  }
+  test(){
+    alert('推出的登陆')
+  }
   render () {
     const props = this.props
     const Item = List.Item
     const Brief = Item.Brief
     return props.user?(
       <div>
+      <Button type="ghost" onClick={this.test}>注册</Button>
        <Result
         img={<img src={require(`../images/${props.avatar}.png`)} className="avatar" alt=""/>}
         title={props.user}
@@ -24,6 +36,7 @@ class User extends React.Component{
            {props.desc.split('\n').map(v => {
              return <Brief key={v}>{v}</Brief>
            })}
+           {props.money?<Brief>薪资：{props.money}</Brief>:null}
          </Item>
        </List>
       </div>
